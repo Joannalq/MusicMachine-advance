ec16fcab24daa8ee8bb578e92edc64831d3c02b3
Joannalq <joanna851026@hotmail.com>
2017-06-02 17:56:52 +1000

    create handler

---
+++ app/src/main/java/com/example/joanna/musicmachine_advance/PlayHandler.java
@@ -0,0 +1,38 @@
+package com.example.joanna.musicmachine_advance;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.RemoteException;
+
+
+public class PlayHandler extends Handler {
+    private PlayerService mPlayerService;
+
+    public PlayHandler (PlayerService playerService){
+        mPlayerService=playerService;
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+       switch (msg.arg1){
+           case 0://play
+               mPlayerService.play();
+               break;
+
+           case 1://pause
+               mPlayerService.pause();
+               break;
+
+           case 2://isplaying
+               int isplaying=mPlayerService.isPlaying()?1:0;
+               Message message=Message.obtain();
+               message.arg1=isplaying;
+               try {
+                   msg.replyTo.send(message);
+               } catch (RemoteException e) {
+                   e.printStackTrace();
+               }
+
+       }
+    }
+}
